1504ae6dabf2665d5535439ee9b8b9afbabd96d3
Kacper Home <kacperhome@Kacpers-MacBook-Pro.local>
2022-11-04 20:43:15 +0100

    change app context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import { useReducer } from 'react'
+import { reducer, defaultAppContextState } from './context/AppContext'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AppContext } from './context/AppContext'
 import { LoadingView } from './views/LoadingView/LoadingView'
+import { MapView } from './views/MapView/MapView'
 import { ScanQRView } from './views/ScanQRView/ScanQRView'
 
 
 function App() {
-
-return (
-   <AppContext.Provider value= {{title: 'USOS.WEB.APP'}}>
-      <Router>
-        <Route path="/" element={<ScanQRView title="ScanQRView"/>} />
-        <Route path="/" element={<LoadingView />} />
-      </Router> 
-    </AppContext.Provider>
-  )
+  
+  const [state, dispatch] = useReducer(reducer, defaultAppContextState);
+  return (
+    <AppContext.Provider value={{state, dispatch}} >
+        <Router>
+          <Route path="/" element={<ScanQRView title="ScanQRView"/>} />
+          <Route path="/Loading" element={<LoadingView />} /> 
+          <Route path="/Map" element={<MapView />} />
+        </Router> 
+      </AppContext.Provider>
+    )
 }
+
 export default App
