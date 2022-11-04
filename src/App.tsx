126695c332555c1d1de729af7097737e551a79b3
Hoaqin <michaldomasik@wp.pl>
2022-11-04 19:17:56 +0100

    FIx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { AppContext } from './context/AppContext'
 import { LoadingView } from './views/LoadingView/LoadingView'
 import { ScanQRView } from './views/ScanQRView/ScanQRView'
 
-
 function App() {
-
-return (
-   <AppContext.Provider value= {{title: 'USOS.WEB.APP'}}>
+  return (
+    <AppContext.Provider value={{ appTitle: 'USOS.WEB.APP' }}>
       <Router>
-        <Route path="/" element={<ScanQRView title="ScanQRView"/>} />
+        <Route path="/" element={<ScanQRView title="ScanQRView" />} />
         <Route path="/" element={<LoadingView />} />
-      </Router> 
+      </Router>
     </AppContext.Provider>
   )
 }
