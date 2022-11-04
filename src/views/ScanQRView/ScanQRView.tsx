8cdbc5749930172b04dc390515167d79998773a4
Hoaqin <michaldomasik@wp.pl>
2022-11-04 23:18:00 +0100

    Input for qrCode

--- src/views/ScanQRView/ScanQRView.tsx
+++ src/views/ScanQRView/ScanQRView.tsx
@@ -1,6 +1,9 @@
 import { useAppContext } from '../../context/AppContext'
 import { QrReader } from 'react-qr-reader'
 import { useNavigate } from 'react-router-dom'
+import { Header } from '../../components/Header/Header'
+import { Button } from '../../components/Button/Button'
+import { useState } from 'react'
 
 interface ScanQRViewProps {
   title: string
@@ -9,11 +12,14 @@ interface ScanQRViewProps {
 export const ScanQRView = ({ title }: ScanQRViewProps) => {
   const { state, dispatch } = useAppContext()
   let navigate = useNavigate()
-
+  const [value, setVal] = useState()
+  const handleInput = (e: any) => {
+    const val = e.target.value
+    setVal(val)
+  }
   return (
     <>
-      <div>{title}</div>
-      <div>{state.appTitle}</div>
+      <Header title="kutas" />
       <QrReader
         onResult={(result, error) => {
           if (result) {
@@ -26,6 +32,12 @@ export const ScanQRView = ({ title }: ScanQRViewProps) => {
         }}
         constraints={{}}
       />
+
+      <div className="downPanel" style={{ textAlign: 'center' }}>
+        <p>...lub wpisz liczbę pod kodem QR:</p>
+        <input placeholder="6 cyfrowy kod" onChange={e => handleInput(e)}></input>
+        <Button text="Submit" onClick={() => navigate('/search/' + value)}></Button>
+      </div>
     </>
   )
 }
