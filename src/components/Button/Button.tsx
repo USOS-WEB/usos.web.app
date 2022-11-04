b693cbd2eba2b79407e14eabeb0a0e568b0f1cf4
jagtym <jagtymxd@gmail.com>
2022-11-04 19:58:18 +0100

    create button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.css';
+
+interface ButtonProps {
+    text: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({text}) => {
+    return (
+        <button className={styles.button}>{text}</button>
+    );
+}
