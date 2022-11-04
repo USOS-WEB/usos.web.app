e5619c633541885039ac8219340bd436b3c7eaff
Kacper Home <kacperhome@Kacpers-MacBook-Pro.local>
2022-11-04 18:37:18 +0100

    cleanup project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+interface HeaderProps {
+    title: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({title}) => {
+    return <header className={styles.container}>{title}</header>
+}
