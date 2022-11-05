836cf8aab533ff3e291d9b2074058eaabe7266b0
Hoaqin <michaldomasik@wp.pl>
2022-11-05 03:45:33 +0100

    gdzie jestem

--- src/views/PlaceDetailsView/PlaceDetailsView.tsx
+++ src/views/PlaceDetailsView/PlaceDetailsView.tsx
@@ -1,23 +1,35 @@
+import { useNavigate } from 'react-router-dom'
+import { Button } from '../../components/Button/Button'
+import { Header } from '../../components/Header/Header'
 import searchMock from '../../mocks/searchMock.json'
 
 const PlaceDetailsView = () => {
   const getData = () => {
     return JSON.parse(JSON.stringify(searchMock))
   }
+  let navigate = useNavigate()
+
   return (
     <>
-      <h2>{getData().path.name}</h2>
+      <Header title="Uniwersalny System Ochrony Studentów" />
+      <h2>{getData().path[0].name}</h2>
       <br />
-      <img src={getData().path.img}></img>
+      <img src={getData().path[0].img}></img>
       <br />
-      <p>{getData().path.description}</p>
+      <p>{getData().path[0].description}</p>
       <br />
       <p style={{ fontWeight: 'bold' }}>Lista pięter</p>
       <ul>
-        {getData().path.floors.map((floor: any) => (
-          <li>{floor.text}</li>
+        {getData().path[0].floors.map((floor: any) => (
+          <li>{floor}</li>
         ))}
       </ul>
+      <Button
+        text="Wróć do widoku trasy"
+        onClick={() => {
+          navigate('/Path')
+        }}
+      ></Button>
     </>
   )
 }
